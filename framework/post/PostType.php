71163c8d5810822efbedf36ab440d35b712a17dc
Tooraj Khatibi <2rajpx@gmail.com>
2016-07-07 00:01:50 +0430

    Updated structure

---
+++ framework/post/PostType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace assistant\post;
+
+class PostType extends PostTypeBase {
+	
+	/**
+	 * Sortable
+	 * Define what columns are sortable in the admin edit screen.
+	 * @param array $columns An array of columns that are sortable.
+	 * @return PostType Current object
+	 */
+	// public function sortable(array $columns = []){
+	// 	// Assign user defined sortable columns to object variable.
+	// 	$this->sortable = $columns;
+	// 	return $this;
+	// }
+	
+}
